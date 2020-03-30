3b424636bef36418aca3de074479392da0385076
Harry Pengelly <harrs@btinternet.com>
2020-03-30 13:07:31 -0700

    Add files via upload

---
+++ Coding Task Csharp/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+
+namespace CodingTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+
+            while (running)
+            {
+                int invalid, valid;
+                string[] inputList;
+                invalid = 0;
+                valid = 0;
+
+                Console.Write("Patient ID(s):");
+                string input = Console.ReadLine();
+
+
+                if (input == "url")
+                {
+                    input = new System.Net.WebClient().DownloadString("https://s3.amazonaws.com/cognisant-interview-resources/identifiers.json");
+                    input = input.Trim('[', ']');
+                    input = input.Replace("\"", string.Empty);
+                    inputList = input.Split(",");
+
+                }
+                else if (input == "exit")
+                {
+                    running = false;
+                    break;
+                }
+                else
+                {   
+                    inputList = new string[] { input };
+                }
+
+                foreach (string id in inputList)
+                {
+                    if (VerifyID(id))
+                    {
+                        valid++;
+                    }
+                    else
+                    {
+                        invalid++;
+                    }
+                }
+
+                Console.WriteLine("Valid Results ["+valid+"]");
+                Console.WriteLine("Inavlid Results [" + invalid + "]");
+                Console.WriteLine("Total IDs: [" + (valid + invalid) + "]\n");
+                Console.WriteLine("Press any key to input another ID(s)");
+                Console.ReadKey();
+                Console.Clear();
+            }         
+        }
+
+        static bool VerifyID(string id)
+        {
+            if (id.Length != 10 || !IsDigits(id) || IdenticalChars(id))
+            {
+                return false;
+            }
+            if (!WeightCheck(id))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static bool IsDigits(string id)
+        {
+            return id.All(char.IsDigit);
+        }
+
+        static bool IdenticalChars(string id)
+        {
+            return id.All(ch => ch == id[0]);
+        }
+
+        static bool WeightCheck(string id)
+        {
+            
+            int[] weights = { 11, 10, 9, 8, 7, 6, 5, 4, 3 };
+            int[] weightedId = new int[9];
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weightedId[i] = weights[i] * int.Parse(id[i].ToString());
+            }
+ 
+
+            int weightSum = weightedId.Sum();
+            int remainder = weightSum % 12;
+            int finalDigit = 12 - remainder;
+
+            if (finalDigit == 11)
+            {
+                finalDigit = 0;
+            }
+
+            return (finalDigit == int.Parse(id[9].ToString()));
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,34 @@
+# Coding Challenge
+
+Simple application to determine validity of patient IDs for certain conditions.
+
+## Installation
+
+To Build application install Visual Studio 2019 and locate "Coding Task Csharp.sln" in extracted dir.
+
+Build using Visual Studio, exe found in release.
+
+
+## Usage
+
+For windows, find directory of extracted package i.e.
+
+```bash
+C:\Users\harrs\source\repos\Coding Task Csharp\Coding Task Csharp\bin\Release\netcoreapp3.1
+```
+Right click 'Coding Task Csharp.exe' and select **Open**
+
+OR
+
+If building, locate release folder with built exe.
+```
+## Commands
+
+Input string eg "12312321j21g3hh" to test individual IDs.
+
+Input "url" to test imported array from given url for task.
+
+Input "exit" to exit application.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/) © Harry Pengelly
